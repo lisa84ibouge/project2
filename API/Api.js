f70e24d458cbb97a205987bdd825e9bfd58add86
dmh34 <dmhalljr87@outlook.com>
2019-03-31 08:22:44 -0700

    comments added.

--- API/Api.js
+++ API/Api.js
@@ -2,20 +2,34 @@ const axios = require("axios");
 
 
 class API {
-    constructor (apikey){
+    constructor(apikey) {
         this.API_KEY = apikey;
     }
 
-    async getFlights(city){
-        let airport = await axios.get(" http://aviation-edge.com/v2/public/autocomplete?key="+this.API_KEY+"&city="+city);
-        let flights = await axios.get(" http://aviation-edge.com/v2/public/timetable?key="+this.API_KEY+"&iataCode="+airport.data.cities[0].codeIataCity+"&type=departure");
-        return flights.data;
+    /**
+     * 
+     * @param {*} params
+     *  
+     */
+    async MakeAPICall(params) {
+        try {
+            let airport = await axios.get(" http://aviation-edge.com/v2/public/autocomplete?key=" + this.API_KEY + "&city=" + params);
+            let flights = await axios.get(" http://aviation-edge.com/v2/public/timetable?key=" + this.API_KEY + "&iataCode=" + airport.data.cities[0].codeIataCity + "&type=departure");
+            return flights.data;
+        }catch{
+            throw "An error ocurred";
+            
+        }
     }
-    
-    
-}
 
-function CreateAPI(apikey){
+
+}
+/**
+ * Creates the api object.
+ * @param {String} apikey 
+ * 
+ */
+function CreateAPI(apikey) {
     return new API(apikey);
 }
 
