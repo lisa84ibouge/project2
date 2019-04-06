afff61abacc5737ecadc965048ad4bfcb5368062
Lyly <yhl4340@gmail.com>
2019-04-06 11:18:47 -0700

    else condition is running

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,9 @@
 var db = require("../models");
 var Op = db.Sequelize.Op;
 
-module.exports = function(app) {
+module.exports = function (app) {
   // read the info at this path
-  app.get("/api/users", function(req, res) {
+  app.get("/api/users", function (req, res) {
     console.log(req.body, "req body?");
     // req.query is the result of the query
     console.log(req.query);
@@ -11,88 +11,53 @@ module.exports = function(app) {
       where: {
         city: req.query.city
       }
-    }).then(function(users) {
+    }).then(function (users) {
       // result is the result of query
       console.log(users);
       res.json(users);
     });
   });
+
   // create
   app.post("/api/user/", function (req, res) {
+    let temp;
     db.User.findAll({
       where: {
         [Op.or]: [{ city: req.body.city }, { country: req.body.country }],
       },
     }).then(function (matchingUsers) {
-      console.log('here ---->')
-      for (var i = 0; i < matchingUsers.length; i++) {
-        if (matchingUsers[i].city == req.body.city) {
-          console.log('matching name:', matchingUsers[i].name, ': ', matchingUsers[i].city, 'city');
-          // matching city is working. logging out matching city
-        } else if (matchingUsers[i].country == req.body.country) {
-          console.log('Matching country: ', matchingUsers[i].country, 'matching name: ', matchingUsers[i].name)
-        } else {
-          // the else condition doesn't run
-          console.log('No matches found!');
-          res.redirect('/questions')
+      temp = matchingUsers;
+      console.log('here ---->', temp.length)
+      // if there is a match, then do the for loop
+      if (matchingUsers.length > 0) {
+        for (var i = 0; i < matchingUsers.length; i++) {
+          if (matchingUsers[i].city == req.body.city) {
+            console.log('matching name:', matchingUsers[i].name, ': ', matchingUsers[i].city, 'city');
+            // matching city is working. logging out matching city
+          } else {
+            console.log('Matching country: ', matchingUsers[i].country, 'matching name: ', matchingUsers[i].name);
+          }
         }
-      } db.User.create({
-        name: req.body.name,
-        city: req.body.city,
-        photo: req.body.photo,
-        age: req.body.age,
-        lang: req.body.lang,
-        country: req.body.country,
-        secLang: req.body.secLang
-      }).then(function (dbUser) {
-        // no need to run the for loop because the 'where' clause already filters 
-        res.json(matchingUsers);
-      })
-    });
-  });
-
-//   app.post("/api/user/", function(req, res) {
-//     db.User.create({
-//       name: req.body.name,
-//       city: req.body.city,
-//       state: req.body.state,
-//       photo: req.body.photo,
-//       age: req.body.age,
-//       lang: req.body.lang,
-//       country: req.body.country,
-//       secLang: req.body.secLang
-//     }).then(function(dbUser) {
-//       db.User.findAll({
-//         where: {
-//           //   $or:[
-//           //     {
-//           //     city:req.body.city
-//           //   }, {
-//           //     country:req.body.country
-//           //   }
-//           // ],
-//           // id:{$not: req.body.id}
-//           [Op.or]: [{ city: req.body.city }, { country: req.body.country }]
-//           // id:{
-//           //   [Op.not]:[req.body.id]
-//           // }
-//         }
-//       }).then(function(matchingUsers) {
-//         console.log("here ---->", matchingUsers);
-//         for (let i = 0; i < matchingUsers.length; i++) {
-//           if (!matchingUsers[i].city) {
-//             console.log(
-//               "matching:",
-//               matchingUsers[i].name,
-//               ": ",
-//               matchingUsers[i].city
-//             );
-//           } else {
-//             console.log(matchingUsers[i].country, " country matched");
-//             res.json(matchingUsers[i].country);
-//           }
-//         }
-//       });
-//     });
-//   });
- };
+      } else {
+        // the else condition doesn't run
+        console.log('No matches found!');
+        // res.redirect('/questions');
+      };
+    }).then(function() {
+      db.User.create({
+      name: req.body.name,
+      city: req.body.city,
+      photo: req.body.photo,
+      age: req.body.age,
+      lang: req.body.lang,
+      country: req.body.country,
+      secLang: req.body.secLang
+    }).then(function () {
+      console.log('temp here-----', temp)
+      // no need to run the for loop because the 'where' clause already filters 
+      // res.json(temp);
+      res.send(temp)
+    })
+  })
+});
+};
