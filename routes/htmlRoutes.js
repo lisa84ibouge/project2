905d8692e37fbe987d2662711442327ed57f9111
Delbert Hall <dmhalljr87@outlook.com>
2019-04-13 09:39:45 -0700

    contact html updated

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -27,6 +27,10 @@ module.exports = function (app) {
     resp.sendFile(path.join(__dirname, "../public/html/login.html"));
   })
 
+  app.get("/contact", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/contact.html"));
+  })
+
 
 
 }
